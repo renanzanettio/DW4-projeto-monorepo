75796fb6e3c65d871e323aabd2ae7b5188d05212
DSM4 <fatec-dsm4@academico.intra>
2026-08-17 22:19:51 -0300

    17/08/2026

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,6 +21,36 @@ app.get('/api/health', (req: Request, res: Response) => {
     });
 });
 
+// Cadastrar novo usuário
+app.post('/api/users', async (req: Request, res: Response) => {
+    try {
+
+        const { nome, email, senha_hash } = req.body;
+
+        if (!nome || !email || !senha_hash) {
+            return res.status(400).json({ erro: 'nome, email e senha_hash são obrigatórios!' })
+        }
+
+        const novoUsuario = await User.create({ nome, email, senha_hash });
+
+        return res.status(201).json(novoUsuario)
+
+    } catch (error: any) {
+        return res.status(500).json({ erro: 'Erro ao cadastrar usuário.', detalhe: error.message })
+    }
+});
+
+//Listar todos os usuários
+app.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const usuarios = await User.findAll({
+            attributes: ['id', 'nome', 'email', 'createdAt']
+        });
+        return res.status(200).json(usuarios)
+    } catch (error: any) {
+        return res.status(500).json({ erro: 'Erro ao listar usuário.', detalhe: error.message })
+    }
+})
 
 
 async function main() {
